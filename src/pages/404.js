e00ee6fd42574001d6be2615217b48f6a4610f0c
Sophie <48594165+sophiemcgarity@users.noreply.github.com>
2022-10-31 13:45:41 -0700

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -16,7 +16,7 @@ export default function NotFound() {
             <Heading as="h1">Page not found</Heading>
             <Flex variant="column" gap={0}>
               <Text variant="lead" className={styles.text}>
-                Sorry! We couldn’t find the page you were looking for.
+                Sorry! We couldn’t find the page you were looking for!!!! Test
               </Text>
               <Link to="/" className={styles.link}>
                 <span>Back to home</span>
